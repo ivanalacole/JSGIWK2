1f85228ff8a1b01ae031de07e1e0ade0b8fc51c2
Ivana Burton Thompson <IvanaBT@R2Hs-MBP.lan>
2022-04-20 15:20:02 -0400

    pushed finished GI to repo

---
+++ easy.js
@@ -0,0 +1,11 @@
+//EASY: Create an array of students holding their 
+//last name, first name, and age with 3 students. To
+//validate, please log a greeting with the first name, 
+//last name and age of the 2nd student. The output
+//should look like "Hello, my name is John Doe and I'm 19 years old."
+
+let peer1 = ['Ivana', 'BT', 24]
+let peer2 = ['Hayden', 'Roche', 35]
+let peer3 = ['Ashley', 'Lorenzo', 20]
+
+console.log(`Hello my name is ${peer2[0]} ${peer2[1]} and I am ${peer2[2]}.`)
---
+++ veryEasy.js
@@ -0,0 +1,12 @@
+//VERY EASY: Write a function named min that takes 
+//two arguments and returns their minimum.
+               //paramater
+function min (num1, num2) {
+    if( num1 < num2) {
+    return num1
+} else{
+    return false
+}
+}
+console.log(min(4,10));
+            //argument
